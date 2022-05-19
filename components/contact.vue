3c68a2ccf92d9a58e69ca8bad5988403c76a8ec3
Fassi Théo <gtheya@gmail.com>
2022-05-19 09:19:50 +0200

    [IMP] Placeholder for wip project and contact tab

--- components/contact.vue
+++ components/contact.vue
@@ -1,6 +1,10 @@
 <template>
-<div class="tab" id="contact">
-        <div class="title mx-auto">Contact</div>
+  <div class="tab" id="contact">
+    <div class="title mx-auto">Contact</div>
     <h1 class="header">Me Contacter</h1>
-</div>
+    <p class="italic text-center mt-4 mb-2">
+      La partie contact sera <span class="text-green-500">bientôt disponible</span>. <br />
+      Si vous voulez me contacter, vous pouvez m'envoyer un email a <a class="underline text-green-500" href="mailto:fassitheo@protonmail.com">fassitheo@protonmail.com</a>
+    </p>
+  </div>
 </template>
--- components/projects.vue
+++ components/projects.vue
@@ -2,5 +2,6 @@
 <div class="tab" id="projects">
     <div class="title mx-auto">Projets</div>
     <h1 class="header">Mes Projets</h1>
+    <p class="italic text-center mt-4 mb-2">La liste de mes projets sera <span class="text-green-500">bientôt disponible</span>.</p>
 </div>
 </template>
