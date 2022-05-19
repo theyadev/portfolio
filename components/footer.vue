3f0034846e5380ae5b1d7a0b6ad063ad325f9fa8
Fassi Théo <gtheya@gmail.com>
2022-05-19 15:39:30 +0200

    [IMP] Target blank

--- components/footer.vue
+++ components/footer.vue
@@ -2,10 +2,10 @@
   <div class="bg-zinc-900 text-white px-8 md:px-40 py-10">
     <div class="flex items-center gap-x-2 italic text-sm">
       <p>Made with</p>
-      <a href="https://v3.nuxtjs.org/">
+      <a href="https://v3.nuxtjs.org/" target="_blank">
         <img src="~/assets/images/Nuxt_icon.svg" class="h-6 w-6" />
       </a>
-      <a href="https://tailwindcss.com/">
+      <a href="https://tailwindcss.com/" target="_blank">
         <img src="~/assets/images/tailwindcss_icon.svg" class="h-6 w-6" />
       </a>
       <p>by <a class="underline underline-offset-1" href="https://github.com/theyadev">Fassi Théo</a></p>
