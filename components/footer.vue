e1f5c441f4d8640dbbff98e811ed1861a8aa02c5
Fassi Théo <gtheya@gmail.com>
2022-05-23 11:22:04 +0200

    [IMP] Added copyright

--- components/footer.vue
+++ components/footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-zinc-900 text-white px-8 md:px-40 py-10">
+  <div class="bg-zinc-900 text-white px-8 md:px-40 py-5 flex flex-col ">
     <div class="flex items-center gap-x-2 italic text-sm">
       <p>Made with</p>
       <a href="https://v3.nuxtjs.org/" target="_blank">
@@ -10,5 +10,6 @@
       </a>
       <p>by <a class="underline underline-offset-1" href="https://github.com/theyadev">Fassi Théo</a></p>
     </div>
+    <p class="text-xs italic text-center mt-6 s">© Théo Fassi. All rights reserved.</p>
   </div>
 </template>
