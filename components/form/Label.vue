babc3b342b8f7fd2000ad6c5fa1cfdb262c0c0f0
Fassi Théo <gtheya@gmail.com>
2022-05-29 11:23:37 +0200

    [IMP] added for to props

--- components/form/Label.vue
+++ components/form/Label.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 type Props = {
   value: string;
+  for: string;
 };
 const props = defineProps<Props>();
 </script>
 
 <template>
   <label
-    for="name"
+    :for="props.for"
     class="absolute top-0 font-semibold transition-all duration-500 peer-focus:-translate-y-8 peer-focus:ml-0 peer-focus:text-white"
     :class="value.length > 0 ? 'text-white -translate-y-8 ml-0' : 'text-gray-500 ml-4 translate-y-2'"
   >
